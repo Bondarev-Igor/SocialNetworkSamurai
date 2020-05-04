7efe6f1c7b086558e4604fc37133cee7c40c287e
Bondarev-Igor <tosca@tut.by>
2020-05-04 02:52:50 -0700

    edit avatar

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -8,7 +8,7 @@ const Profile = () => {
         <div className={style.content}>
             <img
                 src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIB3QFozh1_t1Kc6YvfKM73Q0aUswu-l3jilKsl2k8UvLyV32u&usqp=CAU'
-                alt="Image"/>
+                alt="avatar"/>
             <div>
                 ava+description
             </div>
