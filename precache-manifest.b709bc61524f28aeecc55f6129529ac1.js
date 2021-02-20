0d590463b0aa133b073d1fbd96b303b6a18cf1df
Bondarev-Igor <tosca@tut.by>
2021-02-20 23:16:54 +0300

    Updates

---
+++ precache-manifest.b709bc61524f28aeecc55f6129529ac1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d002349184d6e5272d5618de9aa2c8",
+    "url": "/socialnetworksamurai/index.html"
+  },
+  {
+    "revision": "583a56e0d6500e345493",
+    "url": "/socialnetworksamurai/static/css/3.d2258aec.chunk.css"
+  },
+  {
+    "revision": "8d5e181798469cb5f2b5",
+    "url": "/socialnetworksamurai/static/css/4.8d6253fe.chunk.css"
+  },
+  {
+    "revision": "1552d19469a98c72ff7f",
+    "url": "/socialnetworksamurai/static/css/main.3eb4a923.chunk.css"
+  },
+  {
+    "revision": "b25da03efbd9ea607d1e",
+    "url": "/socialnetworksamurai/static/js/2.3102c125.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialnetworksamurai/static/js/2.3102c125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583a56e0d6500e345493",
+    "url": "/socialnetworksamurai/static/js/3.bed56040.chunk.js"
+  },
+  {
+    "revision": "8d5e181798469cb5f2b5",
+    "url": "/socialnetworksamurai/static/js/4.293104ee.chunk.js"
+  },
+  {
+    "revision": "1552d19469a98c72ff7f",
+    "url": "/socialnetworksamurai/static/js/main.aea09e46.chunk.js"
+  },
+  {
+    "revision": "a5ca1304df47ddfcb696",
+    "url": "/socialnetworksamurai/static/js/runtime-main.0e5f12c1.js"
+  },
+  {
+    "revision": "0163e1816025ec826d72ed722ec09d4d",
+    "url": "/socialnetworksamurai/static/media/ava.0163e181.jpg"
+  },
+  {
+    "revision": "5ebe3408630f9d977be08f8ad9756b1d",
+    "url": "/socialnetworksamurai/static/media/preloader.5ebe3408.gif"
+  }
+]);
