ee2de5e9eae606d406a1bd401bf003d8be60aef1
Bondarev-Igor <tosca@tut.by>
2021-03-09 21:27:15 +0300

    add Info of User in ProfileInfo.tsx.

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './ProfileInfo.module.css';
 import {ProfileType} from "../ProfileContainer";
 import Preloader from "../../common/Preloader/Preloader";
@@ -14,7 +14,19 @@ type PropsType = {
     updateStatus: (status: string) => void
 }
 
+type ProfileDataType = {
+    profile: ProfileType
+    isOwner: boolean
+    goToEditMode:() => void
+}
+type ProfileDataFormType = {
+    profile: ProfileType
+}
+
 const ProfileInfo = (props: PropsType) => {
+
+    let [editMode, setEditMode] = useState(false)
+
     // пока профиля нет - верни Preloader
     if (!props.profile) {
         return <Preloader/>
@@ -30,35 +42,61 @@ const ProfileInfo = (props: PropsType) => {
             <div className={style.descriptionBlock}>
                 <img src={props.profile.photos.large || userPhoto} className={style.mainPhoto}/><br/>
                 {props.isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
-
-                <div>
-                    <div>
-                        <b>Full name:</b> {props.profile.fullName}
-                    </div>
-                    <div>
-                        <b>Looking for a job:</b> {props.profile.lookingForAJob ? 'yes' : 'no'}
-                    </div>
-                    {props.profile.lookingForAJob &&
-                    <div>
-                        <b>My professional skills:</b> {props.profile.lookingForAJobDescription}
-                    </div>}
-                    <div>
-                        <b>About me:</b> {props.profile.aboutMe}
-                    </div>
-                    <div>
-                        <b>Contacts:</b> {Object.keys(props.profile.contacts).map((key) =>{
-                            return <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key]} />
-                    })}
-                    </div>
-                </div>
-
+                { editMode
+                    ? <ProfileDataForm profile={props.profile}/>
+                    : <ProfileData goToEditMode = {() => setEditMode(true)} profile={props.profile} isOwner={props.isOwner} /> }
                 <ProfileStatusWithHooks status={props.status}
                                         updateStatus={props.updateStatus}/>
             </div>
         </div>
     )
 }
-
+const ProfileData = (props: ProfileDataType) => {
+    return <div>
+        {props.isOwner && <div><button onClick={props.goToEditMode}>edit</button></div>}
+        <div>
+            <b>Full name:</b> {props.profile.fullName}
+        </div>
+        <div>
+            <b>Looking for a job:</b> {props.profile.lookingForAJob ? 'yes' : 'no'}
+        </div>
+        {props.profile.lookingForAJob &&
+        <div>
+            <b>My professional skills:</b> {props.profile.lookingForAJobDescription}
+        </div>}
+        <div>
+            <b>About me:</b> {props.profile.aboutMe}
+        </div>
+        <div>
+            <b>Contacts:</b> {Object.keys(props.profile.contacts).map((key) => {
+            return <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>
+        })}
+        </div>
+    </div>
+}
+const ProfileDataForm = (props: ProfileDataFormType) => {
+    return <div>
+        Form
+        {/*<div>*/}
+        {/*    <b>Full name:</b> {props.profile.fullName}*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*    <b>Looking for a job:</b> {props.profile.lookingForAJob ? 'yes' : 'no'}*/}
+        {/*</div>*/}
+        {/*{props.profile.lookingForAJob &&*/}
+        {/*<div>*/}
+        {/*    <b>My professional skills:</b> {props.profile.lookingForAJobDescription}*/}
+        {/*</div>}*/}
+        {/*<div>*/}
+        {/*    <b>About me:</b> {props.profile.aboutMe}*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*    <b>Contacts:</b> {Object.keys(props.profile.contacts).map((key) => {*/}
+        {/*    return <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>*/}
+        {/*})}*/}
+        {/*</div>*/}
+    </div>
+}
 
 const Contacts = ({contactTitle, contactValue}: any) => {
     return <div className={style.contact}><b>{contactTitle}</b>: {contactValue}</div>
