2f2bd99d58f1ddf03ed4e683d5d7e79fb3817d59
Bondarev-Igor <tosca@tut.by>
2021-04-16 23:23:56 +0300

    add in appReducer some types

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -3,11 +3,15 @@ import {getAuthUserData} from "./auth-reducer";
 
 const INITIALIZED_SUCCESS = "INITIALIZED_SUCCESS";
 
-let initialState = {
+type InitialStateType = {
+    initialized: boolean
+}
+
+let initialState: InitialStateType = {
     initialized: false,
 };
 
-const appReducer = (state = initialState, action: any) => {
+const appReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
     switch (action.type) {
         case INITIALIZED_SUCCESS :
             return {
@@ -19,7 +23,11 @@ const appReducer = (state = initialState, action: any) => {
     }
 };
 
-export const initializedSuccess = () => ({type: INITIALIZED_SUCCESS});
+type InitializedSuccessActionType = {
+    type: typeof INITIALIZED_SUCCESS
+}
+
+export const initializedSuccess = (): InitializedSuccessActionType => ({type: INITIALIZED_SUCCESS});
 
 export const initializeApp = () => (dispatch: any) => {
     let promise = dispatch(getAuthUserData());
